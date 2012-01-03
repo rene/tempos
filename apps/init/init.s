68c43c96c05414576f3751fc3a52e85c818163d8
Renê de Souza Pinto <rene@renesp.com.br>
2012-01-03 16:26:11 -0200

    Introduce the new source code TempOS tree.

---
+++ apps/init/init.s
@@ -0,0 +1,27 @@
+/*
+ * Simple program to run on TempOS
+ * Actually, this is our first user program!
+ *
+ * Author: Renê de Souza Pinto
+ * Date:   03/01/2012
+ */
+
+.global _start
+
+/* Note: Program starts at 10MB (linker will handle this)*/
+
+.text
+_start:
+	movl $0x04, %eax   /* syscall number  */
+	movl $0x01, %ebx   /* fd              */
+	movl $msg,  %ecx   /* buffer          */
+	movl $0x1D, %edx   /* size            */
+	int  $0x80         /* make a syscall  */
+
+/* Never exit from init.... */
+loop:
+	jmp loop
+
+msg:
+	.ascii "Hello world from Userspace!\n"
+
