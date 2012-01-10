1412e11c003a2f89138e1fab0886e33ce6d3d573
Renê S. Pinto <rene@renesp.com.br>
2012-01-10 09:41:58 -0200

    Add GRUB2 official support (without workarounds).

--- kernel/scripts/do_mkdisk.sh
+++ kernel/scripts/do_mkdisk.sh
@@ -10,34 +10,10 @@
 
 if [ $# != 2 ]; then
 	echo "Use:"
-	echo "$0 <tempos_kernel_image> <floppy_disk_image>"
+	echo "$0 <tempos_kernel_image> <cd_iso_image>"
 	echo
 	exit 1
 fi
 
-##
-# Check for genext2fs
-#
-echo -n " + Checking for genext2fs..." 
-GENEXT2FS=$(which genext2fs 2> /dev/null)
-if [ $? -eq 0 ]; then
-	echo "OK"
-	./scripts/mkdisk_img.sh $1 $2 $GENEXT2FS
-else
-	# genext2fs not present, check for root user
-	echo "not found."
-	echo -n " ! genext2fs not present. Checking for root user..."
-	WHOAMI=$(whoami)
-
-	if [ $WHOAMI == "root" ]; then
-		echo "OK"
-		./scripts/mkdisk_img_with_root.sh $1 $2
-	else
-		echo "non root"
-		echo " ERROR: genext2fs was not found in your system. You should run 'make install'"
-		echo "        as root to create floppy disk image."
-		echo " >> You can download genext2fs from http://genext2fs.sourceforge.net/"
-		exit 1
-	fi
-fi
+./scripts/mkdisk_img.sh $1 $2
 
--- kernel/scripts/mkdisk_img.sh
+++ kernel/scripts/mkdisk_img.sh
@@ -16,6 +16,7 @@ check_result() {
 		echo "ERROR!"
 		echo "Log output:"
 		[ -n "$errorlog" ] && cat $errorlog || echo "No error log avaliable."
+		rm $errorlog
 		exit 1
 	fi
 }
@@ -23,7 +24,13 @@ check_result() {
 # Make image using grub 1
 use_grub1() {
 	
-	GRUB=$(which grub)
+	MKISOFS=$(which mkisofs 2> /dev/null)
+
+	if [ -z "$MKISOFS" ]; then
+		echo "ERROR: mkisofs not found. Please, install cdrtools."
+		echo "http://cdrecord.berlios.de/private/cdrecord.html"
+		exit 1
+	fi
 
 	mktemp=$(which mktemp)
 	if [ -n $mktemp ]; then
@@ -32,7 +39,7 @@ use_grub1() {
 		errorlog="/tmp/$$"
 	fi
 
-	echo -n " + Creating floppy root directory..."
+	echo -n " + Creating CD-ROM root directory..."
 	TMPDIR=$($mktemp -d)
 	if [ $? != 0 ]; then
 		TMPDIR=/tmp/$$-dir
@@ -44,13 +51,11 @@ use_grub1() {
 	mkdir -p $TMPDIR/boot/grub
 	cp $TEMPOSFILE $TMPDIR/boot 2>>$errorlog
 	check_result
-	echo -n " + Copying GRUB files..."
-	cp /boot/grub/stage* $TMPDIR/boot/grub 2>>$errorlog
+	echo -n " + Copying GRUB boot image..."
+	cp /boot/grub/stage2_eltorito $TMPDIR/boot/grub 2>>$errorlog
 	check_result
 
-	DEVICEMAP=$(mktemp)
-	CMDFILE=$(mktemp)
-
+	echo -n " + Creating GRUB configuration file..."
 	cat > $TMPDIR/boot/grub/menu.lst << EOF
 ##
 # TempOS
@@ -59,35 +64,21 @@ use_grub1() {
 color white/blue light-green/black
 
 title   TempOS
-root    (fd0)
 kernel  /boot/tempos.elf root=3:1 init=/sbin/init
 EOF
-
-echo "(fd0)		$IMGFILE" > $DEVICEMAP
-cat > $CMDFILE << EOF
-root (fd0)
-install /boot/grub/stage1 d (fd0) (fd0)/boot/grub/stage2 0x8000 p (fd0)/boot/grub/menu.lst
-quit
-EOF
-
-	echo -n " + Creating ext2 file system image..."
-	$GENEXT2FS -d $TMPDIR -b1536 $IMGFILE >>$errorlog 2>&1
 	check_result
 
-	echo -n " + Installing GRUB..."
-
-	grep -v ^# $CMDFILE | $GRUB --batch --device-map=$DEVICEMAP > /dev/null 2>>$errorlog
+	echo -n " + Creating bootable GRUB ISO image..."
+	$MKISOFS -R -b boot/grub/stage2_eltorito -no-emul-boot \
+		-boot-load-size 4 -boot-info-table -o $IMGFILE $TMPDIR > $errorlog 2>&1
 	check_result
 
-	rm -f $DEVICEMAP
-	rm -f $CMDFILE
-	rm -rf $TMPDIR
+	rm -rf $TMPDIR 2> /dev/null
+	rm -f $errorlog 2> /dev/null
 }
 
 # Make image using grub 2
 use_grub2() {
-	# FIXME: This is a workaround to make floppy disk with
-	#        GRUB1 on systems with GRUB2 installed.
 
 	mktemp=$(which mktemp)
 	if [ -n $mktemp ]; then
@@ -96,7 +87,7 @@ use_grub2() {
 		errorlog="/tmp/$$"
 	fi
 
-	echo -n " + Creating floppy root directory..."
+	echo -n " + Creating CD-ROM root directory..."
 	TMPDIR=$($mktemp -d)
 	if [ $? != 0 ]; then
 		TMPDIR=/tmp/$$-dir
@@ -105,52 +96,53 @@ use_grub2() {
 	check_result
 
 	echo -n " + Copying TempOS kernel image..."
-	mkdir -p $TMPDIR/boot
+	mkdir -p $TMPDIR/boot/grub
 	cp $TEMPOSFILE $TMPDIR/boot 2>>$errorlog
 	check_result
-	
-	echo -n " + Uncompressing GRUB1 files..."
-	tar -xjf $GRUB1TARBALL -C $TMPDIR/boot
-	mv $TMPDIR/boot/grub1.bin /tmp
+
+	echo -n " + Creating GRUB configuration file..."
+	cat > $TMPDIR/boot/grub/menu.lst << EOF
+##
+# TempOS
+#
+menuentry "TempOS" {
+	multiboot /$(basename $TEMPOSFILE) root=3:1 init=/sbin/init
+}
+
+COLOR_NORMAL="white/blue"
+COLOR_HIGHLIGHT="light-green/black"
+EOF
 	check_result
 
-	echo -n " + Creating ext2 file system image..."
-	$GENEXT2FS -d $TMPDIR -b1536 $IMGFILE >>$errorlog 2>&1
+	echo -n " + Creating bootable GRUB ISO image..."
+	$GRUBMKR -o $IMGFILE $TMPDIR > $errorlog 2>&1
 	check_result
 
-	echo -n " + Installing GRUB..."
-	dd if=/tmp/grub1.bin of=$IMGFILE conv=notrunc bs=512 count=1 >>$errorlog 2>&1
-	check_result
-
-	rm /tmp/grub1.bin
-	rm -rf $TMPDIR
+	rm -rf $TMPDIR 2> /dev/null
+	rm -f $errorlog 2> /dev/null
 }
 
 # main
-if [ $# != 3 ]; then
+if [ $# != 2 ]; then
 	echo "Use:"
-	echo "$0 <tempos_kernel_image> <floppy_disk_image> <path to genext2fs>"
+	echo "$0 <tempos_kernel_image> <iso_cd_image>"
 	echo
 	exit 1
 else
 	TEMPOSFILE=$1
 	IMGFILE=$2
-	GENEXT2FS=$3
 fi
 
-# GRUB1 files
-GRUB1TARBALL=scripts/grub1.tar.bz2
 
 # Check grub's version
-GRUBI=$(which grub-install)
-if [ -z "$GRUBI" ]; then
-	echo "ERROR: GRUB not found. Trying to use GRUB2..."
-	use_grub2
-else
-	GVERSION=$($GRUBI -v | sed "s/.*\([0-9]\.[0-9][0-9]\).*/\1/")
+GRUBI=$(which grub-install 2> /dev/null)
+GRUBMKR=$(which grub-mkrescue 2> /dev/null)
+if [ -z "$GRUBI" -a -z "$GRUBMKR" ]; then
+	echo "ERROR: GRUB not found."
+	exit 1
 fi
 
-if [ ${GVERSION:0:1} -eq 0 ]; then
+if [ -z "$GRUBMKR" ]; then
 	# GRUB1
 	use_grub1
 else
